0c27c6e15665bd023d9cce2754c9527a1163b4ba
Aljumaine Gayle <37164902+aljumaine@users.noreply.github.com>
2021-08-25 19:04:08 -0400

    Updates
    Updates to overall CSS and animation mouse movement speed. Updated all objects and background images.

--- objects.js
+++ objects.js
@@ -5,28 +5,30 @@ $(function() {
     let px2 = $(".px-bg-2");
     let halfWidth = window.innerWidth / 2;
     let halfHeight = window.innerHeight / 2;
-    function onWindowResize(){
-      halfWidth = window.innerWidth / 2;
-      halfHeight = window.innerHeight / 2;
+
+    function onWindowResize() {
+        halfWidth = window.innerWidth / 2;
+        halfHeight = window.innerHeight / 2;
     }
     window.addEventListener("resize", onWindowResize);
     let _mouseX = 0;
     let _mouseY = 0;
     // Magic happens here
     function mousemove(e) {
-      _mouseX = e.clientX;
-      _mouseY = e.clientY;
+        _mouseX = e.clientX;
+        _mouseY = e.clientY;
     }
+
     function loop() {
-      if (_mouseX && _mouseY) {
-        let x_norm = _mouseX / halfWidth - 1;
-        let y_norm = _mouseY / halfHeight - 1;
-        let _depth1 = {x: x_norm * 2, y: y_norm * 2};
-        let _depth2 = {x: x_norm * 3, y: y_norm * 3};
-        gsap.to(px1, {xPercent: _depth1.x, yPercent: _depth1.y, duration: 2, ease: 'sine.out'});
-        gsap.to(px2, {xPercent: _depth2.x, yPercent: _depth2.y, duration: 2, ease: 'sine.out'});
-      }
-      requestAnimationFrame(loop);
+        if (_mouseX && _mouseY) {
+            let x_norm = _mouseX / halfWidth - 1;
+            let y_norm = _mouseY / halfHeight - 1;
+            let _depth1 = { x: x_norm * 9, y: y_norm * 9 };
+            let _depth2 = { x: x_norm * 9, y: y_norm * 9 };
+            gsap.to(px1, { xPercent: _depth1.x, yPercent: _depth1.y, duration: 1, ease: 'sine.out' });
+            gsap.to(px2, { xPercent: _depth2.x, yPercent: _depth2.y, duration: 1, ease: 'sine.out' });
+        }
+        requestAnimationFrame(loop);
     }
     requestAnimationFrame(loop);
 });
