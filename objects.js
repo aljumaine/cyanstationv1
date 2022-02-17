82ce8df139f0910db0de2403f7d19b2c2946ffab
Aljumaine Gayle <37164902+aljumaine@users.noreply.github.com>
2022-02-16 20:38:05 -0500

    Updating Size of Logo
    Updated the size of the logo.

--- objects.js
+++ objects.js
@@ -23,7 +23,7 @@ $(function() {
         if (_mouseX && _mouseY) {
             let x_norm = _mouseX / halfWidth - 1;
             let y_norm = _mouseY / halfHeight - 1;
-            let _depth1 = { x: x_norm * 9, y: y_norm * 9 };
+            let _depth1 = { x: x_norm * 8, y: y_norm * 8 };
             let _depth2 = { x: x_norm * 2, y: y_norm * 2 };
             gsap.to(px1, { xPercent: _depth1.x, yPercent: _depth1.y, duration: 0.5, ease: 'sine.out' });
             gsap.to(px2, { xPercent: _depth2.x, yPercent: _depth2.y, duration: 0.5, ease: 'sine.out' });
